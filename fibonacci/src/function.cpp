65fea642b76f3a896465aa6efdaf9d4c7714a069
pablodamascenoo <pablo_damasceno@DESKTOP-JFNLS96.localdomain>
2021-06-11 15:51:19 -0300

    fibonacci concluído

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -3,7 +3,17 @@
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
     // TODO: adicione o seu código aqui.
-
+    std::vector<unsigned int> v = {};
+    if(n == 1){
+        return v;
+    }
+    else{
+        v.push_back(1);
+        v.push_back(1);
+    }
+    while (v[v.size()-1]+v[v.size()-2]< n){
+        v.push_back(v[v.size()-1]+v[v.size()-2]);
+    }
     // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    return v;
 }
